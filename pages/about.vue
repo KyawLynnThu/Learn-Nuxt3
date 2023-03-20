d988b4e9ab3f155c9f418179d7e1b26feac6d006
kyawlinnthu <kyawlinnthu1999@gmail.com>
2023-03-20 09:05:12 +0630

    adding pages

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut ut, cum similique, officia expedita provident dicta, suscipit necessitatibus deserunt dolorum nostrum? Totam, deserunt recusandae a nihil debitis maiores voluptatum reiciendis!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus dolores quia sequi repellat odit sunt obcaecati earum laboriosam accusamus dolor suscipit atque rerum voluptatum, alias quod autem ullam ipsum temporibus!</p>
+    </div>
+</template>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+p {
+    margin: 20px 0;
+}
+</style>
